165c4d29dd21a20c18ff859fc08eb36bc65e5a25
falldeaf <falldeaf@gmail.com>
2021-02-15 03:36:40 -0500

    First commit, almost ready to test flying

---
+++ index.js
@@ -0,0 +1,247 @@
+'use strict';
+
+require('dotenv').config();
+const delay = require('await-delay');
+const wifi = require('node-wifi');
+const ping = require('ping');
+const express = require('express');
+const app = express();
+const port = 8000;
+const sdk = require('tellojs');
+
+var connected_to_drone = false;
+
+//Video Websocket
+const http = require('http');
+const WebSocketServer = require('websocket').server;
+const server = http.createServer();
+server.listen(5544);
+const video_ws = new WebSocketServer({
+	httpServer: server
+});
+var video_clients = [];
+video_ws.binaryType = 'arraybuffer';
+video_ws.on('request', function(request) {
+	const connection = request.accept(null, request.origin);
+	video_clients.push(connection) - 1;
+
+	connection.on('connection', function(video_ws) {
+		console.log("new conn.");
+		console.log(video_ws.clients);
+	});
+});
+video_ws.broadcast = function(data) {
+	video_clients.forEach(client => client.send(data));
+};
+
+//Bidirectional Comms Websocket
+const http2 = require('http');
+const WebSocketServer2 = require('websocket').server;
+const server2 = http2.createServer();
+server2.listen(5533);
+const comms_ws = new WebSocketServer2({
+	httpServer: server2
+});
+var comms_clients = [];
+comms_ws.on('request', function(request) {
+	const connection = request.accept(null, request.origin);
+	comms_clients.push(connection) - 1;
+	connection.on('message', function(message) {
+		var client_message_obj = JSON.parse(message.utf8Data);
+		switch(client_message_obj.type) {
+			case "command":
+				runCommand(client_message_obj);
+				break;
+		}
+	});
+
+	connection.on('connection', function(comms_ws) {
+		console.log("new conn.");
+		console.log(comms_ws.clients);
+	});
+
+	connection.on('close', function(reasonCode, description) {
+		console.log('Client has disconnected.');
+	});
+});
+comms_ws.broadcast = function(data) {
+	comms_clients.forEach(client => client.send(data));
+};
+
+
+wifi.init({
+	iface: null // network interface, choose a random wifi interface if set to null
+});
+
+async function runCommand(comm) {
+	console.log(comm);
+	sendLog(`Saw ${comm.name} command`);
+	switch(comm.name) {
+		case "takeoff":
+			await sdk.control.takeOff();
+			break;
+		case "land":
+			await sdk.control.land();
+			break;
+		case "emergency":
+			await sdk.control.emergency();
+			break;
+		case "stop":
+			await sdk.control.stop();
+			break;
+		case "up":
+			await sdk.control.move.up(comm.val)
+			break;
+		case "down":
+			await sdk.control.move.down(comm.val);
+			break;
+		case "left":
+			await sdk.control.move.left(comm.val);
+			break;
+		case "right":
+			await sdk.control.move.right(comm.val);
+			break;
+		case "front":
+			await sdk.control.move.front(comm.val);
+			break;
+		case "back":
+			await sdk.control.move.back(comm.val);
+			break;
+		case "rotateleft":
+			await sdk.control.rotate.clockwise(comm.val);
+			break;
+		case "rotateright":
+			await sdk.control.rotate.counterClockwise(comm.val);
+			break;
+		case "flipleft":
+			await sdk.control.flip.left();
+			break;
+		case "flipright":
+			await sdk.control.flip.right();
+			break;
+		case "flipfront":
+			await sdk.control.flip.front();
+			break;
+		case "flipback":
+			await sdk.control.flip.back();
+			break;
+	}
+}
+
+var h264chunks = []
+var numChunks = 3
+var numChunkz = 0
+async function turnOnVideo() {
+	const videoEmitter = await sdk.receiver.video.bind() 
+	videoEmitter.on('message', data => {
+		var idx = data.indexOf(Buffer.from([0, 0, 0, 1]))
+		if (idx > -1 && h264chunks.length > 0) {
+			h264chunks.push(data.slice(0, idx))
+			numChunkz = numChunkz + 1
+			if (numChunkz === numChunks) {
+				video_ws.broadcast(Buffer.concat(h264chunks));
+				h264chunks = []
+				numChunkz = 0
+			}
+			h264chunks.push(data.slice(idx)) 
+		} else {
+			h264chunks.push(data)
+		}
+	});
+}
+
+app.get('/video', (req, res) => {
+	res.send('Trying to connect and command');
+	sdk.control.connect()
+	.then(() => turnOnVideo());
+	//.then((result) => console.log(result))
+	//.catch((error) => console.error(error))
+});
+
+app.get('/connect/:post', async (req, res) => {
+	res.send('connecting...');
+
+	
+	while(!await scan('TELLO')){ await delay(3000); }
+	await delay(2000);
+	await connectCommand();
+	while(!await pingtest('192.168.10.1')){ await delay(1000); }
+	await sdk.control.connect();
+	console.log('drone fully connected');
+
+	const stateEmitter = await sdk.receiver.state.bind();
+	stateEmitter.on('message', res => comms_ws.broadcast('{"tele":' + JSON.stringify(res) + '}'));
+
+	if(req.params.post === 'video') turnOnVideo();
+});
+
+app.get('/disconnect', (req, res) => {
+	res.send('disconnecting...');
+	wifi.disconnect(error => {
+		if (error) {
+			console.log(error);
+		} else {
+			console.log('Disconnected');
+		}
+	});
+})
+
+app.use(express.static('static'))
+
+app.use('/scripts', express.static(__dirname + '/node_modules/jmuxer/dist/'));
+
+app.listen(port, () => {
+	console.log(`App ready at http://localhost:${port}/index.html`)
+});
+
+async function connectCommand() {
+	console.log('connecting to Tello WIFI AP...');
+	return new Promise((resolve, reject) => {
+		wifi.connect({ ssid: process.env.ssid, password: process.env.pass }, async error => {
+			if (error) { console.log(error); }
+			console.log('Connected');
+			sendLog("connected");
+			await delay(1000);
+			return resolve(error);
+		});
+	});
+}
+
+function pingtest(host) {
+	return new Promise((resolve, reject) => {
+		ping.sys.probe(host, function (isAlive) {
+			var msg = isAlive ? 'host ' + host + ' is alive' : 'host ' + host + ' is dead';
+
+			sendLog(msg);
+			console.log(msg);
+			return resolve(isAlive);
+		});
+	});
+}
+
+function scan(search) {
+	return new Promise((resolve, reject) => {
+		wifi.scan((err, networks) => {
+				if (err) {
+					return reject(err);
+				}
+				var string_contains = false;
+				networks.some(network => {
+					if(network.ssid.includes(search)) {
+						console.log(search + " found!");
+						sendLog(search + " found!");
+						return resolve(true);
+					}
+				});
+				sendLog(search + " not found");
+				console.log(search + " not found");
+				return resolve(false);
+			}
+		);
+	});
+}
+
+function sendLog(message) {
+	console.log(message);
+	comms_ws.broadcast('{"pong":"' + message + '"}');
+}
---
+++ static/3d_drone.js
@@ -0,0 +1,106 @@
+var roll = 0;
+var pitch = 0;
+var yaw = 0;
+(async function () {
+	//3D Drone Telemetry
+	const loader = new THREE.GLTFLoader();
+	var scene3d = document.getElementById("telemetry");
+	var width = 300;
+	var height = 300;
+	scene = new THREE.Scene();
+
+	// CAMERA 
+	//camera = new THREE.PerspectiveCamera(60, width / height, 0.1, 100);
+	camera = new THREE.OrthographicCamera( 5, -5, 5, -5, 100, 1 );
+	camera.position.x = 10;
+	camera.position.y = -10;
+	camera.position.z = 10;
+	camera.lookAt(scene.position);
+
+	// RENDERER
+	renderer = new THREE.WebGLRenderer({alpha: true});
+	renderer.setClearColor( 0x000000, 0 ); // the default
+	renderer.setSize(width, height);
+
+	// GEOMETRY & MATERIALS
+	const l1material = new THREE.LineDashedMaterial( {
+		color: 0xFF7F9A,
+		dashSize: .5,
+		gapSize: .5,
+	} );
+	const l1geometry = new THREE.BufferGeometry().setFromPoints( [ new THREE.Vector3(-5, 0.1, 0),  new THREE.Vector3(5, 0.1, 0)] );
+	const xline = new THREE.Line( l1geometry, l1material );
+	xline.computeLineDistances();
+	scene.add( xline );
+
+	const l2material = new THREE.LineDashedMaterial( {
+		color: 0xC2EE00,
+		dashSize: .5,
+		gapSize: .5,
+	} );
+	//const l2material = new THREE.LineBasicMaterial( { color: 0xff0000 } );
+	const l2geometry = new THREE.BufferGeometry().setFromPoints( [ new THREE.Vector3(0, -5.1, 0),  new THREE.Vector3(0, 5.1, 0)] );
+	const yline = new THREE.Line( l2geometry, l2material );
+	yline.computeLineDistances();
+	scene.add( yline );
+
+	const l3material = new THREE.LineDashedMaterial( {
+		color: 0x6DBCF3,
+		dashSize: .5,
+		gapSize: .5,
+	} );
+	const l3geometry = new THREE.BufferGeometry().setFromPoints( [ new THREE.Vector3(0, 0.1, -5),  new THREE.Vector3(0, 0.1, 5)] );
+	const zline = new THREE.Line( l3geometry, l3material );
+	zline.computeLineDistances();
+	scene.add( zline );
+
+	const geometry = new THREE.CircleGeometry( 5, 32 );
+	const material = new THREE.MeshPhongMaterial({color: 0xFF0000, opacity: 0.5, transparent: true});
+	const circle = new THREE.Mesh( geometry, material );
+	scene.add( circle );
+	circle.rotateX( - Math.PI / 2);
+
+	//Drone
+	const gltf = await modelLoader('/drone_model.glb', loader);
+	scene.add(gltf.scene);
+
+	// LIGHT
+	var spot1 = new THREE.SpotLight(0xffffff);
+	spot1.position.set(-20, 1, 10);
+	scene.add(spot1);
+
+	const light = new THREE.DirectionalLight(0xFFFFFF, 1);
+	light.position.set(6, -50, -5);
+	light.target.position.set(10, 0, 10);
+	scene.add(light);
+	scene.add(light.target);
+
+	scene3d.appendChild(renderer.domElement);
+	renderer.render(scene, camera);
+
+	setInterval(render,1000/30);
+
+	function render() {
+		//gltf.scene.rotation.z -= 0.02;
+		gltf.scene.rotation.x = THREE.Math.degToRad(pitch);
+		gltf.scene.rotation.y = THREE.Math.degToRad(yaw);
+		gltf.scene.rotation.z = THREE.Math.degToRad(roll);
+
+		if(props_on) {
+			gltf.scene.children.forEach(child => {
+				if(child.name.includes('prop')) {
+					child.rotation.y += 1;
+				}
+			});
+		}
+
+		//requestAnimationFrame(render);
+		renderer.render(scene, camera);
+	}
+})();
+
+function modelLoader(url, loader) {
+	return new Promise((resolve, reject) => {
+		loader.load(url, data=> resolve(data), null, reject);
+	});
+}
